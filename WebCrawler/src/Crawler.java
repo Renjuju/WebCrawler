e388026ff7b55420ec3caeffd469b18771863fa7
Renju R <renju467@gmail.com>
2016-01-09 19:24:20 -0500

    Added html parsing

--- WebCrawler/src/Crawler.java
+++ WebCrawler/src/Crawler.java
@@ -1,5 +1,26 @@
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Scanner;
+
 /**
  * Created by Renju R on 1/9/2016.
  */
 public class Crawler {
+    public String getHtml(String url) {
+        String htmlContent = null;
+        URLConnection connection = null;
+        try {
+            connection = new URL(url).openConnection();
+            Scanner scanner = new Scanner(connection.getInputStream());
+            scanner.useDelimiter("\\Z");
+            htmlContent = scanner.next();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return htmlContent;
+    }
 }
--- WebCrawler/src/WebCrawler.java
+++ WebCrawler/src/WebCrawler.java
@@ -3,6 +3,7 @@
  */
 public class WebCrawler {
     public static void main(String[] args) {
-
+        Crawler myCrawler = new Crawler();
+        System.out.println(myCrawler.getHtml("http://google.com"));
     }
 }
