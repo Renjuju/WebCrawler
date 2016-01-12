bde9d473f7045e8fa1d07dbf6fd2244a7858e5cc
renju <renju467@gmail.com>
2016-01-11 19:35:00 -0500

    Refactor Crawler

--- WebCrawler/src/Crawler.java
+++ WebCrawler/src/Crawler.java
@@ -3,14 +3,17 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Stack;
+import java.util.Vector;
 
 /**
  * Created by Renju R on 1/9/2016.
  */
 public class Crawler {
 
+
 //    http://stackoverflow.com/questions/8616781/how-to-get-a-web-pages-source-code-from-java
-    public String grabHtml(String url)  {
+    private String grabHtml(String url)  {
         StringBuilder html = null;
         try{
             URL urlObj = new URL(url);
@@ -27,5 +30,20 @@ public class Crawler {
         }
 
         return html.toString();
+    }
+
+    public String crawl(int levels, String url) {
+        String html = grabHtml(url);
+        Parser htmlParser = new Parser();
+        Vector<String> urlVector = htmlParser.getHtmlUrls(html);
+        Stack<Vector<String>> fatStacks = new Stack<>();
+        fatStacks.push(urlVector);
+        for(int x = 0; x < levels; x++) {
+            for(int i = 0; i < urlVector.size(); i++) {
+                System.out.println(urlVector.get(i));
+            }
+        }
+
+        return "";
     }
 }
--- WebCrawler/src/WebCrawler.java
+++ WebCrawler/src/WebCrawler.java
@@ -6,15 +6,8 @@ import java.util.Vector;
  */
 public class WebCrawler {
     public static void main(String[] args) {
-        Crawler myCrawler = new Crawler();
-        Parser  myParser  = new Parser();
 
-        String html = myCrawler.grabHtml("http://www.google.com/imghp?hl=en&tab=wi");
-        Vector<String> urls = myParser.getHtmlUrls(html);
-        for(int i = 0; i < urls.size(); i++) {
-            System.out.println("---------" + i + "----------");
-            String crawlerHtml = myCrawler.grabHtml(urls.get(i));
-            myParser.getHtmlUrls(crawlerHtml);
-        }
+        Crawler webCrawler = new Crawler();
+        String html = webCrawler.crawl(1, "http://www.google.com/imghp?hl=en&tab=wi");
     }
 }
