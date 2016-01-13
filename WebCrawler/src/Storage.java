57a3d03c9ed6ee7f6c60a432b988f494ca650be2
renju <renju467@gmail.com>
2016-01-13 18:08:33 -0500

    Adding url store

--- WebCrawler/src/Storage.java
+++ WebCrawler/src/Storage.java
@@ -1,5 +1,17 @@
+import java.util.Vector;
+
 /**
  * Created by Renju R on 1/9/2016.
  */
 public class Storage {
+
+    private Vector<String> urlStore = new Vector<>();
+
+    public void addToUrlStore(String url) {
+        urlStore.add(url);
+    }
+
+    public Vector<String> getUrlList() {
+        return urlStore;
+    }
 }
